1e2187a4bff11f069203ee682d5a01145a3ff901
Raymund <raymund.catahay@majority.com>
2021-01-23 13:39:15 +0100

    updated package source

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Eventful",
-            dependencies: ["MulticastDelegate"]),
+            dependencies: ["MulticastDelegate"],
+            path: "Sources"),
         .testTarget(
             name: "EventfulTests",
             dependencies: ["Eventful"]),
